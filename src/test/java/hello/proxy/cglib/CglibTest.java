805a39ece02bdb6497ffc5ba9480f947ce652d99
chanboklee <chanboklee@naver.com>
2023-02-26 21:38:08 +0900

    Fix : dynamic proxy -> cglib

---
+++ src/test/java/hello/proxy/cglib/CglibTest.java
@@ -0,0 +1,28 @@
+package hello.proxy.cglib;
+
+import hello.proxy.cglib.code.TimeMethodInterceptor;
+import hello.proxy.common.ConcreteService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.cglib.proxy.Enhancer;
+
+@Slf4j
+public class CglibTest {
+
+    /**
+     * 구체클래스를 상속받아 실행
+     */
+    @Test
+    void cglib(){
+        ConcreteService target = new ConcreteService();
+
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(ConcreteService.class);
+        enhancer.setCallback(new TimeMethodInterceptor(target));
+        ConcreteService proxy = (ConcreteService) enhancer.create();
+        log.info("targetClass -> {}", target.getClass());
+        log.info("proxyClass -> {}", proxy.getClass());
+
+        proxy.call();
+    }
+}
---
+++ src/test/java/hello/proxy/cglib/code/TimeMethodInterceptor.java
@@ -0,0 +1,34 @@
+package hello.proxy.cglib.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class TimeMethodInterceptor implements MethodInterceptor {
+
+    private final Object target;
+
+    public TimeMethodInterceptor(Object target) {
+        this.target = target;
+    }
+
+    /**
+     * 인터페이스 없이 사용 가능
+     * method 를 사용해도 되지만 methodProxy 를 사용하는 것을 권장
+     */
+    @Override
+    public Object intercept(Object obj, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+
+        Object result = methodProxy.invoke(target, args);
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime -> {}", resultTime);
+        return result;
+    }
+}
