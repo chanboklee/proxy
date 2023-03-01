193d2a79b85edd2aea827f89f721fc2517c58152
chanboklee <chanboklee@naver.com>
2023-03-01 23:29:15 +0900

    Fix : spring proxy -> pointcut

--- src/test/java/hello/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/proxy/advisor/AdvisorTest.java
@@ -3,10 +3,16 @@ package hello.proxy.advisor;
 import hello.proxy.common.ServiceImpl;
 import hello.proxy.common.ServiceInterface;
 import hello.proxy.common.advice.TimeAdvice;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+
+import java.lang.reflect.Method;
 
 public class AdvisorTest {
 
@@ -27,5 +33,60 @@ public class AdvisorTest {
         proxy.save();
         proxy.find();
 
+    }
+
+    @Test
+    @DisplayName("직접 만든 포인트컷")
+    void advisorTest2(){
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+
+    }
+
+    static class MyPointcut implements Pointcut {
+
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    /**
+     * isRuntime 이 false 의 경우 matches(Method method, Class<?> targetClass) 호출.. 캐싱이 가능
+     * isRuntime 이 true 의 경우 matches(Method method, Class<?> targetClass, Object... args) 호출.. 캐싱 안한다
+     */
+    @Slf4j
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("포인틐컷 호출 method -> {} targetClass -> {}", method.getName(), targetClass);
+            log.info("result -> {}", result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
     }
 }
